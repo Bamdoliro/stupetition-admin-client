fbc6dab2956c5605345f5c60111b19d7ffe44683
SEOKKAMONI <sj01048260279@gmail.com>
2023-03-05 14:51:34 +0900

    ADD :: Join

--- src/components/layout/home/Main/index.tsx
+++ src/components/layout/home/Main/index.tsx
@@ -1,24 +1,16 @@
-import { useState } from 'react';
-import { JoinType } from 'types/auth.type';
+import { Outlet } from 'react-router-dom';
 import * as S from './style';
 
-const Join = () => {
-  const [joinData, setJoinData] = useState<JoinType>({
-    username: '',
-    password: '',
-    rePassword: '',
-    schoolId: 0,
-    schoolName: '',
-  });
-  const [searchSchoolIsOpen, setSearchSchoolOpen] = useState(true);
-
+const Main = () => {
   return (
     <S.MainLayout>
       <S.Wrap>
-        <S.MainBox />
+        <S.MainBox>
+          <Outlet />
+        </S.MainBox>
       </S.Wrap>
     </S.MainLayout>
   );
 };
 
-export default Join;
+export default Main;
