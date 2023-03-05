d2d043c311cd1394b56ceef4d9e98549376c1441
SEOKKAMONI <sj01048260279@gmail.com>
2023-03-05 16:44:22 +0900

    ADD :: layuts 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import Login from 'components/layout/auth/Login';
 import CreateSchool from 'components/layout/home/CreateSchool';
 import Generate from 'components/layout/home/Generate';
-import MainLayout from 'components/common/Layouts/MainLayout';
+import Main from 'layouts/MainLayout';
 import { Route, Routes } from 'react-router-dom';
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
-      <Route path="/main" element={<MainLayout />}>
+      <Route path="/main" element={<Main />}>
         <Route path="/main/genrate" element={<Generate />} />
         <Route path="/main/create/school" element={<CreateSchool />} />
       </Route>
